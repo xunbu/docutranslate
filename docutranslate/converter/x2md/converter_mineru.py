60dd7f7117872a65d4e7cd126e8118f6d558fad9
xunbu <xunbu3@qq.com>
2026-03-08 19:46:12 +0800

    提高mineru的读取写入时间

--- docutranslate/converter/x2md/converter_mineru.py
+++ docutranslate/converter/x2md/converter_mineru.py
@@ -39,8 +39,8 @@ class ConverterMineruConfig(X2MarkdownConverterConfig):
 
 timeout = httpx.Timeout(
     connect=5.0,  # 连接超时 (建立连接的最长时间)
-    read=200.0,  # 读取超时 (等待服务器响应的最长时间)
-    write=200.0,  # 写入超时 (发送数据的最长时间)
+    read=3600.0,  # 读取超时 (等待服务器响应的最长时间)
+    write=360.0,  # 写入超时 (发送数据的最长时间)
     pool=1.0  # 从连接池获取连接的超时时间
 )
 
